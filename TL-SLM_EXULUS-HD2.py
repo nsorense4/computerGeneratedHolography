abef095b60c07fdc93106565a001497166175b8c
Nicholas Sorensen <nsorense@ualberta.ca>
2023-03-23 16:21:17 -0400

    Updating with the HG mode stuff.

--- TL-SLM_EXULUS-HD2.py
+++ TL-SLM_EXULUS-HD2.py
@@ -20,7 +20,7 @@ import scipy
 import os
 import matplotlib.pyplot as plt
 
-from gaussianModes import LGModeArray
+from gaussianModes import LGModeArray, HGModeArray
 from gerchbergSaxtonAlgorithm import  *
 
 dirPath = str(os.path.dirname(os.path.realpath(__file__))).replace("\\", '/')
@@ -31,37 +31,43 @@ class EXULUS_HD2:
     pitch = 0.008 # mm
     
     def exportPhasePlot(l, m, figName, beamWidth = 1.5, modeWidth = 1, tol = 0.01, 
-                        maxIter = 200, r = 1/2, adh = 0.95):
+                        maxIter = 200, r = 1/2, adh = 0.95, gaussMode = 'lg'):
         # modeWidth and beamWidth are in mm
         
-        
-        farFieldField = LGModeArray(l=l,m=m, N=EXULUS_HD2.yRange, M=EXULUS_HD2.xRange, 
+        if gaussMode == 'lg':
+            farFieldField = LGModeArray(l=l,m=m, N=EXULUS_HD2.yRange, M=EXULUS_HD2.xRange, 
                                     w=modeWidth/EXULUS_HD2.pitch)
-        nearFieldFld, farFieldFld, nearFieldFldFinal, farFieldFldFinal, err = (
-            CGHGaussianInput(farFieldField, tol = tol, maxIter = maxIter, 
+        elif gaussMode == 'hg':
+            farFieldField = HGModeArray(l=l,m=m, N=EXULUS_HD2.yRange, M=EXULUS_HD2.xRange, 
+                                    w=modeWidth/EXULUS_HD2.pitch)
+        nearFieldFldFinal = (CGHGaussianInput(farFieldField, tol = tol, maxIter = maxIter, 
                               goal = 'amp+pha', r = r, adh = 0.9,
                               nearGaussWidth= beamWidth/EXULUS_HD2.pitch/EXULUS_HD2.yRange,
-                              phaGuess = 'flat'))[:]
-        
-        nrows = 2
-        ncols = 2
+                              phaGuess = 'flat'))[3]
 
-        fig, ax = plt.subplots(nrows=nrows, ncols=ncols,figsize=(9.8, 10*nrows/ncols))
-        plt.setp(ax, xticks=[], xticklabels = [], yticks = [], yticklabels = [], aspect = 'equal')
-        fig.subplots_adjust(wspace=0.1, hspace=0.1)
-
-        # plot black backgrounds
-        for i in range(nrows):
-            for j in range(ncols):
-                ax[i,j].imshow(np.abs(farFieldField)*0, cmap = 'hot')
-
-        # plot 
-        plotIntAndPhase(nearFieldFld, ax[0,0])
-        plotIntAndPhase(farFieldFld, ax[0,1])
-        ax[1,0].imshow(np.angle(nearFieldFldFinal), cmap = 'hsv')
-        plotIntAndPhase(farFieldFldFinal, ax[1,1])
         
         exportPlotPhase(nearFieldFldFinal, dirPath + '/figures/', figName + '.bmp')
         
+    def exportPhasePlotFromImageAmp(image, figName, beamWidth = 1.5):
         
+        farFieldField = loadImage(image, dirPath)
+        
+        if np.shape(farFieldField) != (EXULUS_HD2.yRange, EXULUS_HD2.xRange):
+            farFieldField = EXULUS_HD2.padArray(farFieldField)        
+        
+        nearFieldFldFinal = CGHGaussianInput(farFieldField, tol = 0.001, maxIter = 100,
+                                        goal = 'amp',
+                                        nearGaussWidth= beamWidth/EXULUS_HD2.pitch/EXULUS_HD2.yRange,
+                                        phaGuess = 'flat')[3]
+        
+        
+        exportPlotPhase(nearFieldFldFinal, dirPath + '/figures/', figName + '.bmp')
+        
+    def padArray(A):
+        (w,h) = np.shape(A)
+        
+        A = np.pad(A, ((np.floor((EXULUS_HD2.yRange-h)/2).astype('int'),np.ceil((EXULUS_HD2.yRange-h)/2).astype('int')), 
+                       (np.floor((EXULUS_HD2.xRange-w)/2).astype('int'),np.floor((EXULUS_HD2.xRange-w)/2).astype('int'))), 'constant')
+        
+        return A
 
